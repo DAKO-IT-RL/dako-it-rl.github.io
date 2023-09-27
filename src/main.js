54622edc27cc9f4a4487137d3a803255d1e081da
DAKO-IT-RL <l.ruhle@dako-it.com>
2023-09-27 16:33:25 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,23 +1,51 @@
 import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+const loader = new GLTFLoader();
 const renderer = new THREE.WebGLRenderer();
+const controls = new OrbitControls( camera, renderer.domElement );
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+camera.position.set(30, 30, 30)
+camera.lookAt(0, 30, 0)
+controls.update();
 
-camera.position.z = 5;
+// set up ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
+
+// load model
+loader.load( './assets/free_tesla_tequila.glb', function ( gltf ) {
+  gltf.scene.traverse( child => {
+    if ( child.material ) child.material.metalness = 0.1;
+  });
+  gltf.scene.position.set(0, -15, 0)
+	scene.add( gltf.scene );
+}, undefined, function ( error ) {
+	console.error( error );
+} );
+
 
 function animate() {
 	requestAnimationFrame( animate );
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  controls.update();
 	renderer.render( scene, camera );
 }
-animate();
+
+if ( WebGL.isWebGLAvailable() ) {
+
+	// Initiate function or other initializations here
+	animate();
+
+} else {
+
+	const warning = WebGL.getWebGLErrorMessage();
+	document.getElementById( 'container' ).appendChild( warning );
+
+}
